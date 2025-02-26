b1103fb6f2b351e025a48b0318c032c26fbd887d
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-02-26 20:55:39 -0300

    Update C_to_F.py

--- C_to_F.py
+++ C_to_F.py
@@ -1,7 +1,5 @@
 # 17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
 
-# Fórmula: C = (5 * ( F-32) / 9)
-
 f = float(input('Informe a temperatura em farenheit: \n'))
 c = (5 * ( f-32) / 9)
 
