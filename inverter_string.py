a6fd5e085f11a871071e219467901b856ce75f42
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-03-10 23:00:56 -0300

    Update inverter_string.py

--- inverter_string.py
+++ inverter_string.py
@@ -2,12 +2,12 @@
 =======
 # **Inversão de String** 🔄: Inverta uma string.
 
-def check_pal(word):
+def invert_string(word):
     invert = word[::-1]
     return invert
     
 word = input('Qual palavra você deseja inverter? \n')
 
-print(check_pal(word))
+print(invert_string(word))
 
 >>>>>>> ee7e2453731774811351953957f9e9186259d453:inverter_string.py
