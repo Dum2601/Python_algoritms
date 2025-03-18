4b7d679376e8746b65ca8668b63a520e47ffc5c0
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-18 19:57:44 -0300

    Create reajuste.py

---
+++ reajuste.py
@@ -0,0 +1,7 @@
+# 6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+
+valor = float(input('Informe o valor que receberá o reajuste: \n'))
+
+calc = valor + (valor * 0.05)
+
+print(calc)
