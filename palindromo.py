c34ee70cb4b041ea1b0822f073f439150c4c1709
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-20 23:37:46 -0300

    Improving with OOP

--- palindromo.py
+++ palindromo.py
@@ -1,15 +1,29 @@
 # 11. **Palíndromo** 🔁: Verifique se uma string é um palíndromo.
 
-def pal_check(word):
-    inverted_name = word[::-1]
-    if inverted_name == word:
-        print('A palavra é um palíndromo!')
-    elif inverted_name != word:
-        print('A palavra não é um palíndromo!')
-    else:
-        print('Informe uma palavra válida!')
-        pal_check(word)
+class Palindromo:
+    
+    def __init__(self, word):
+        self.word = word
+    
+    def pal_check(self):
+        try:
+            inverted_name = self.word[::-1]
+        except Exception as err:
+            print(f'Erro: {err}')
+        
+        try:    
+            if inverted_name == self.word:
+                print('A palavra é um palíndromo!')
+            elif inverted_name != self.word:
+                print('A palavra não é um palíndromo!')
+            else:
+                print('Informe uma palavra válida!')
+                pal_check(self.word)
+        except Exception as err:
+            print(f'Erro: {err}')
 
 name = input('Informe a palavra: \n')
 
-pal_check(name)
+palindromo = Palindromo(name)
+
+palindromo.pal_check()
