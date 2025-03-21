4a60a032026aac409151883d483d2137ed809782
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-20 21:06:21 -0300

    Improving reajuste.py

--- reajuste.py
+++ reajuste.py
@@ -1,7 +1,20 @@
 # 6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
 
-valor = float(input('Informe o valor que receberá o reajuste: \n'))
+def reajust(num):
+    try:
+        
+        post_reajust = num * 0.05
+        
+    except Exception as err:
+        
+        print(f'Aconteceu o seguinte erro: {err} \n',
+        'Tente novamente!'
+        )
+        reajust(num)
+    
+    return post_reajust
 
-calc = valor + (valor * 0.05)
 
-print(calc)
+number = float(input('Informe que receberá o reajuste: \n'))
+
+print(reajust(number))
