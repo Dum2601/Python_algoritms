55f89c28b9ef21b01ead72c80e60df11962d53d2
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-26 21:41:22 -0300

    Improving with OOP

--- C_to_F.py
+++ C_to_F.py
@@ -1,21 +1,21 @@
-# # 17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+# 17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
 
 class Converter:
-    def __init__(self, c):
-        self.c = c
     
-    def celsius(self):
+    def __init__(self, f):
+        self.f = f
+        self.c = 5 * (f-32) / 9
+    
+    @property
+    def show_c(self):
         try:
-            return 5 * (f-32) / 9
+            return self.c
         except Exception as err:
             print(f'Erro: {err}')
+    
+f = float(input('Informe a temperatura em Fahrenheit a ser convertida; \n'))
 
-f = float(input('Informe a temperatura em farenheit: \n'))
+conv = Converter(f)
 
-converter = Converter(f)
-
-
-
-print(converter.celsius())
-        
-        
+print(f'A temperatura em Fahrenheit é: {f}ºF. \n',
+f'A temperatura em Celsius é: {conv.show_c}ºC')
