3ce91bb28b1415adfe339d3e33c9709344c2bacf
Douglas Barbosa <136455750+Dum2601@users.noreply.github.com>
2025-03-26 21:39:24 -0300

    Improving with OOP

--- sort.py
+++ sort.py
@@ -1,10 +1,27 @@
 # Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
 
-a = int(input('Informe o primeiro valor inteiro: \n'))
-b = int(input('Informe o segundo valor inteiro \n'))
-c = int(input('Informe o terceiro valor inteiro \n'))
-
-numbers = [a, b, c]
-numbers.sort(reverse = True)
-
-print(numbers)
+class Sort:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def inverter(self):
+        self._list = [self.a, self.b, self.c]
+        try:
+            self._list.sort(reverse=True)
+        except Exception as err:
+            print(f'Erro: {err}')
+        return self._list
+        
+    @property
+    def show(self):
+        return self.inverter()
+try:        
+    a = int(input('Informe o primeiro número: \n'))
+    b = int(input('Informe o segundo número: \n'))
+    c = int(input('Informe o terceiro número: \n'))
+except Exception as err:
+    print(f'Erro: {err}')
+sort = Sort(a, b, c)
+print(sort.show)
